4c91a9c5211670923fc3b5e7987207f84ca8f312
Angel Aguilar <mgordillo0@ucol.mx>
2020-06-18 12:48:41 -0500

    Graph Change

--- js/code.js
+++ js/code.js
@@ -1,26 +1,28 @@
-var temp;
-var volt;
-function variables(){
-    var particle = new Particle();
-    var token;
+var particle = new Particle();
+var temp = 0;
+var volt = 0;
+var token;
 
-    particle.login({username: 'sgarcia44@ucol.mx', password: 'kakaroto26485'}).then(
+function variables(){
+    particle.login({username: 'USERNAME', password: 'PASSWORD'}).then(
     function(data) {
         token = data.body.access_token;
         var devicesPr = particle.listDevices({ auth: token });
         devicesPr.then(
             function(devices){
             console.log('Devices: ', devices);
-                particle.getVariable({ deviceId: '350024000947373336323230', name: 'temp', auth: token }).then(function(data) {
+                particle.getVariable({ deviceId: 'DEVICEID', name: 'VARIABLE', auth: token }).then(function(data) {
                     console.log('Device variable retrieved successfully:', data);
+                    temp = data.body.result;
                     console.log("Temp: " + data.body.result);
                     document.getElementById("temp").innerHTML = "Temp: " + data.body.result;
                 }, 
                 function(err) {
                     console.log('An error occurred while getting attrs:', err);
                 });
-                particle.getVariable({ deviceId: '350024000947373336323230', name: 'volt', auth: token }).then(function(data) {
+                particle.getVariable({ deviceId: 'DEVICEID', name: 'VARIABLE', auth: token }).then(function(data) {
                     console.log('Device variable retrieved successfully:', data);
+                    volt = data.body.result;
                     console.log("Volt: " + data.body.result);
                     document.getElementById("volt").innerHTML = "Volt: " + data.body.result;
                 }, 
@@ -40,286 +42,44 @@ function variables(){
 }
 //setInterval(variables, 1000);
 
+google.charts.load('current', {'packages':['gauge']});
+google.charts.setOnLoadCallback(drawChartTemp);
+google.charts.setOnLoadCallback(drawChartVolt);
 
-
-window.feed = function(callback) {
-    var particle = new Particle();
-    var token;
-
-    particle.login({username: 'sgarcia44@ucol.mx', password: 'kakaroto26485'}).then(
-    function(data) {
-        token = data.body.access_token;
-        var devicesPr = particle.listDevices({ auth: token });
-        devicesPr.then(
-            function(devices){
-            console.log('Devices: ', devices);
-                particle.getVariable({ deviceId: '350024000947373336323230', name: 'temp', auth: token }).then(function(data) {
-                    console.log('Device variable retrieved successfully:', data);
-                    temp = data.body.result;
-                    console.log("Temp: " + temp);
-                    var tick = {};
-                    tick.plot0 = data.body.result;
-                    callback(JSON.stringify(tick));
-                    document.getElementById("temp").innerHTML = "Temp: " + temp;
-                }, 
-                function(err) {
-                    console.log('An error occurred while getting attrs:', err);
-                });
-            },
-            function(err) {
-            console.log('List devices call failed: ', err);
-            }
-        );
-    },
-    function (err) {
-        console.log('Could not log in.', err);
-    }
-    );
+function drawChartTemp() {
+  var data = google.visualization.arrayToDataTable([
+    ['Label', 'Value'],
+    ['Temperatura', 40],
+  ]);
+  var options = {
+    width: 500, height: 220,
+    redFrom: 40, redTo: 50,
+    yellowFrom:30, yellowTo: 40,
+    minorTicks: 3, max: 50, min: 0
   };
+  var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+  chart.draw(data, options);
+  setInterval(function() {
+    data.setValue(0, 1, temp); 
+    chart.draw(data, options); 
+    }, 1000);
+}
 
-
-  window.feed = function(callback) {
-    var particle = new Particle();
-    var token;
-
-    particle.login({username: 'sgarcia44@ucol.mx', password: 'kakaroto26485'}).then(
-    function(data) {
-        token = data.body.access_token;
-        var devicesPr = particle.listDevices({ auth: token });
-        devicesPr.then(
-            function(devices){
-            console.log('Devices: ', devices);
-                particle.getVariable({ deviceId: '350024000947373336323230', name: 'volt', auth: token }).then(function(data) {
-                    console.log('Device variable retrieved successfully:', data);
-                    volt = data.body.result;
-                    console.log("Volt: " + temp);
-                    var teck = {};
-                    teck.plot0 = data.body.result;
-                    callback(JSON.stringify(teck));
-                    document.getElementById("temp").innerHTML = "Volt: " + volt;
-                }, 
-                function(err) {
-                    console.log('An error occurred while getting attrs:', err);
-                });
-            },
-            function(err) {
-            console.log('List devices call failed: ', err);
-            }
-        );
-    },
-    function (err) {
-        console.log('Could not log in.', err);
-    }
-    );
+function drawChartVolt() {
+  var data = google.visualization.arrayToDataTable([
+    ['Label', 'Value'],
+    ['Voltaje', 5],
+  ]);
+  var options = {
+    width: 500, height: 220,
+    redFrom: 4, redTo: 5,
+    yellowFrom:3, yellowTo: 4,
+    minorTicks: 1, max: 5, min: 0
   };
-
-  
-   
-  var myConfig = {
-    type: "gauge",
-    globals: {
-      fontSize: 10
-    },
-    plotarea: {
-      marginTop: 10
-    },
-    plot: {
-      size: '100%',
-      valueBox: {
-        placement: 'center',
-        text: '%v', //default
-        fontSize: 15,
-        rules: [{
-            rule: '%v >= 20',
-            text: '%v<br>EXCELLENT'
-          },
-          {
-            rule: '%v < 30 && %v > 25',
-            text: '%v<br>Good'
-          },
-          {
-            rule: '%v < 35 && %v > 25',
-            text: '%v<br>Fair'
-          },
-          {
-            rule: '%v >  35',
-            text: '%v<br>Bad'
-          }
-        ]
-      }
-    },
-    tooltip: {
-      borderRadius: 5
-    },
-    scaleR: {
-      aperture: 180,
-      minValue: 10,
-      maxValue: 50,
-      step: 5,
-      center: {
-        visible: false
-      },
-      tick: {
-        visible: false
-      },
-      item: {
-        offsetR: 0,
-        rules: [{
-          rule: '%i == 9',
-          offsetX: 15
-        }]
-      },
-      labels: ['10', '15', '20', '25', '30', '35', '45', '50'],
-      ring: {
-        size: 50,
-        rules: [{
-            rule: '%v <= 20',
-            backgroundColor: '#E53935'
-          },
-          {
-            rule: '%v > 30 && %v < 35',
-            backgroundColor: '#EF5350'
-          },
-          {
-            rule: '%v >= 35 && %v < 25',
-            backgroundColor: '#FFA726'
-          },
-          {
-            rule: '%v >= 35',
-            backgroundColor: '#29B6F6'
-          }
-        ]
-      }
-    },
-    refresh: {
-      type: "feed",
-      transport: "js",
-      url: "feed()",
-      interval: 1500,
-      resetTimeout: 1000
-    },
-    series: [{
-      values: [temp], // starting value
-      backgroundColor: 'black',
-      indicator: [10, 10, 10, 10, 0.75],
-      animation: {
-        effect: 2,
-        method: 1,
-        sequence: 4,
-        speed: 900
-      },
-    }]
-  };
-   
-  zingchart.render({
-    id: 'myChart',
-    data: myConfig,
-    height: 300,
-    width: '100%'
-  });
-
-
-  var myConfig2 = {
-    type: "gauge",
-    globals: {
-      fontSize: 10
-    },
-    plotarea: {
-      marginTop: 10
-    },
-    plot: {
-      size: '100%',
-      valueBox: {
-        placement: 'center',
-        text: '%v', //default
-        fontSize: 15,
-        rules: [{
-            rule: '%v >= 20',
-            text: '%v<br>EXCELLENT'
-          },
-          {
-            rule: '%v < 30 && %v > 25',
-            text: '%v<br>Good'
-          },
-          {
-            rule: '%v < 35 && %v > 25',
-            text: '%v<br>Fair'
-          },
-          {
-            rule: '%v >  35',
-            text: '%v<br>Bad'
-          }
-        ]
-      }
-    },
-    tooltip: {
-      borderRadius: 5
-    },
-    scaleR: {
-      aperture: 180,
-      minValue: 10,
-      maxValue: 50,
-      step: 5,
-      center: {
-        visible: false
-      },
-      tick: {
-        visible: false
-      },
-      item: {
-        offsetR: 0,
-        rules: [{
-          rule: '%i == 9',
-          offsetX: 15
-        }]
-      },
-      labels: ['10', '15', '20', '25', '30', '35', '45', '50'],
-      ring: {
-        size: 50,
-        rules: [{
-            rule: '%v <= 20',
-            backgroundColor: '#E53935'
-          },
-          {
-            rule: '%v > 30 && %v < 35',
-            backgroundColor: '#EF5350'
-          },
-          {
-            rule: '%v >= 35 && %v < 25',
-            backgroundColor: '#FFA726'
-          },
-          {
-            rule: '%v >= 35',
-            backgroundColor: '#29B6F6'
-          }
-        ]
-      }
-    },
-    refresh: {
-      type: "feed",
-      transport: "js",
-      url: "feed()",
-      interval: 1500,
-      resetTimeout: 1000
-    },
-    series: [{
-      values: [volt], // starting value
-      backgroundColor: 'black',
-      indicator: [10, 10, 10, 10, 0.75],
-      animation: {
-        effect: 2,
-        method: 1,
-        sequence: 4,
-        speed: 900
-      },
-    }]
-  };
-   
-  zingchart.render({
-    id: 'myChart2',
-    data: myConfig2,
-    height: 300,
-    width: '100%'
-  });
-
-
+  var chart = new google.visualization.Gauge(document.getElementById('chart_div2'));
+  chart.draw(data, options);
+  setInterval(function() {
+    data.setValue(0, 1, volt); 
+    chart.draw(data, options); 
+    }, 1000);
+}
